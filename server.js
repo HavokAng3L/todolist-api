2427a022beedae7c4f7e42c25716ae25a92f6ab6
HavokAng3L <angelmorin75@gmail.com>
2024-01-18 19:12:06 -0600

    adjusting cors yet again.

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ api.use(Express.json());
 
 api.use(
   cors({
-    origin: "https://main--dynamic-semifreddo-ef4e4f.netlify.app/",
+    origin: "https://main--dynamic-semifreddo-ef4e4f.netlify.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
